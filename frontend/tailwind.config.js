2cb5c4c63e13e7dde6bdec8880be424ff689438e
Jeffrey <jeffrey.rengifom@gmail.com>
2024-07-20 13:10:42 -0500

    confirguarion changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,31 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [],
+  purge: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        bacasime: ['Bacasime Antique', 'serif'],
-        lora: ['Lora', 'serif'],
-        'roboto-mono': ['Roboto Mono', 'monospace'],
-        comfortaa: ['Comfortaa', 'cursive'],
-        josefin: ['Josefin Sans', 'sans-serif'],
-        nunito: ['Nunito', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        quicksand: ['Quicksand', 'sans-serif'],
-        raleway: ['Raleway', 'sans-serif'],
-        roboto: ['Roboto', 'sans-serif'],
-        rubik: ['Rubik', 'sans-serif'],
-        'work-sans': ['Work Sans', 'sans-serif'],
+        bacasime: ["Bacasime Antique", "serif"],
+        lora: ["Lora", "serif"],
+        "roboto-mono": ["Roboto Mono", "monospace"],
+        comfortaa: ["Comfortaa", "cursive"],
+        josefin: ["Josefin Sans", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
+        "work-sans": ["Work Sans", "sans-serif"],
       },
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [nextui()],
 };
